9d67c7c23c76a3ecceb33e6176fb5e22509b8001
maximustlh <110752537+maximustlh@users.noreply.github.com>
2022-08-21 11:01:23 +0800

    Newly updated Ping Pong Game
    Final changes

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,7 @@
 from pygame import *
 
+from pygame import *
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
@@ -41,9 +43,9 @@ clock = time.Clock()
 FPS = 60
 
 #Egg and Spatula
-spatula1 = ("spatula.png", 30, 200, 4, 50, 150)
-spatula2 = ("spatula.png", 520, 200, 4, 50, 150)
-egg = ("egg.png",   200, 200, 4, 50, 50)
+spatula1 = Player("spatula.png", 30, 200, 4, 125, 175)
+spatula2 = Player("spatula.png", 520, 200, 4, 125, 175)
+egg = GameSprite("egg.png", 200, 200, 4, 75, 75)
 
 #Font
 font.init()
@@ -53,14 +55,44 @@ font = font.SysFont("Arial", 35)
 lose1 = font.render("PLAYER 1 - YOU LOSE!", True, (180, 0, 0))
 lose2 = font.render("PLAYER 2 - YOU LOSE!", True, (180, 0, 0))
 
+speed_x = 3
+speed_y = 3
+
 #Gameloop
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    if finish != True:
+        window.fill(back)
+
         spatula1.update_l()
         spatula2.update_r()
+
+        egg.rect.x += speed_x
+        egg.rect.y += speed_y
+
+        #if egg hits the spatula
+        if sprite.collide_rect(spatula1, egg) or sprite.collide_rect(spatula2, egg):
+            speed_x *= -1
+            speed_y *= -1 
+
+        #if ball reaches edge of screen
+        if egg.rect.y > 425 or egg.rect.y < 0:
+            speed_y *= -1
+
+        #if ball reach the left (lose condition for player 1)
+        if egg.rect.x < 0:
+            finish = True
+            window.blit(lose1, (135, 200))
+            game_over = True
+
+        #if ball reach the right (lose condition for player 2) 
+        if egg.rect.x > 600:
+            finish = True
+            window.blit(lose2, (135, 200))
+            game_over = True
 
         spatula1.reset()
         spatula2.reset()
